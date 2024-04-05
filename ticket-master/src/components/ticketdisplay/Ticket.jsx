c5a8b882bba5aaf8a54cb0287c0612516e76ee3d
ElenaiDev <ec22761@qmul.ac.uk>
2024-04-05 18:02:17 +0100

    Update Ticket component to remove reviewer prop and adjust status icons

--- ticket-master/src/components/ticketdisplay/Ticket.jsx
+++ ticket-master/src/components/ticketdisplay/Ticket.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { FaChevronRight } from "react-icons/fa";
+import { FaExclamationCircle, FaTimesCircle, FaClock, FaCheckCircle, FaChevronRight, FaArchive } from "react-icons/fa";
 
-function Ticket({title="No Title Provided", status = "Approved", type = "EC", reviewer = "No Reviewer Assigned", feedback = "No Feedback" }) {
+function Ticket({title="No Title Provided", status = "Approved", type = "EC", feedback = "No Feedback" }) {
     return (
         <div className="card bg-base-100 shadow-xl h-40">
             <div className="flex justify-between items-start p-0 min-h-full">
                 <div className="max-w-80 card-body cursor-pointer flex flex-row justify-between items-center min-h-full gap-10 bg-primary rounded-[1rem_0_0_1rem]">
                     <h2 className='card-title text-xl font-bold text-center text-white self-center line-clamp-3'>{title}</h2>
                 </div>
-                <div className="card-body flex-row max-w-full justify-evenly py-8">
+                <div className="card-body flex-row max-w-full justify-evenly py-8 self-center">
+                    <div className='flex flex-col items-center '>
+                        {status === "Approved" ? <FaCheckCircle size={80} className='text-green-500' />
+                         : status === "Pending" ? <FaClock  size={80} className='text-yellow-500' /> 
+                         : status === "Archived" ? <FaArchive size={80} className='text-primary' />
+                          : <FaTimesCircle size={80} className='text-red-500' />}
+                    </div>
                     <div className='flex flex-col items-center'>
                         <h2 className="card-title font-light text-primary">
                             Status:
@@ -20,12 +26,6 @@ function Ticket({title="No Title Provided", status = "Approved", type = "EC", re
                             Type:
                         </h2>
                         <p className='capitalize'>{type}</p>
-                    </div>
-                    <div className='flex flex-col items-center'>
-                        <h2 className="card-title font-light text-primary text-center">
-                            Reviewed By:
-                        </h2>
-                        <p className='text-center'>{reviewer}</p>
                     </div>
                     <div className='flex flex-col items-center w-72'>
                         <h2 className="card-title font-light text-primary text-center">
